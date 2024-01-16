06602ea1f09bddaf12be8bd4ea9dba8084c91a0b
usr-0123 <fixedzone365@gmail.com>
2024-01-16 17:41:00 +0300

    Submit the rest of the Javascript examples

--- index.js
+++ index.js
@@ -8,9 +8,9 @@
 // **
 // *
 
+console.log("example 4")
 for (let z = 6; z >= 1; z--) {
     console.log(("*").repeat(z));
-    // console.log(z);
 }
 
 // example 5
@@ -23,6 +23,17 @@ for (let z = 6; z >= 1; z--) {
 //   ***
 //    **
 //     *
+console.log("example 5")
+for(let i=1; i<=5; i++){
+    let gap = " ".repeat(5-i);
+    let star = "*".repeat((1+i)-1);
+    console.log(gap + star);
+}
+for(let i=5; i>=1; i--){
+    let gap = " ".repeat(5-i);
+    let star = "*".repeat((1+i)-1);
+    console.log(gap + star);
+}
 
 // example 6
 //     *
@@ -34,6 +45,17 @@ for (let z = 6; z >= 1; z--) {
 //   *****
 //    ***
 //     *
+console.log("example 6")
+for(let i=1; i<=5; i++){
+    let gap = " ".repeat(5-i);
+    let star = "*".repeat((2*i)-1);
+    console.log(gap + star);
+}
+for(let i=5; i>=1; i--){
+    let gap = " ".repeat(5-i);
+    let star = "*".repeat((2*i)-1);
+    console.log(gap + star);
+}
 
 // example 7
 //*******
@@ -44,7 +66,12 @@ for (let z = 6; z >= 1; z--) {
 //      *
 //      *
 //      *
-
+console.log("example 7")
+console.log(("*").repeat(7));
+for (let z = 1; z <= 6; z++) {
+    let gap = " ".repeat(6)
+    console.log(gap + ("*").repeat(1));
+}
 // example 8
 //*******
 //*
@@ -53,3 +80,9 @@ for (let z = 6; z >= 1; z--) {
 //*
 //*
 //*
+console.log("example 8")
+console.log(("*").repeat(7));
+for (let z = 1; z <= 6; z++) {
+    let gap = " ".repeat(6)
+    console.log(("*")+gap.repeat(1));
+}
---
+++ operator-precedence.js
@@ -0,0 +1,17 @@
+// OPERATOR PRECEDENCE IN JS
+//Operator precedence in JavaScript determines the order in which operators are evaluated in an expression. This means that operators with a higher precedence are evaluated first.
+//Parenthesis can be used to specify the order of evaluation, overiding the default precedence.
+// 1. Grouping ()
+// 2. Member Access . []
+// 3. Function Call ()
+// 4. Increment and Decrement ++ --
+// 5. Logical NOT !
+// 6. Multiplication and Division * /
+// 7. Addition and Subtraction + -
+// 8. Relational < > <= >=
+// 9. Equality == != === !==
+// 10. Logical AND &&
+// 11. Logical OR ||
+// 12. Conditional ?:
+// 13. Assignment = += -= *= /= %=
+// 14. Comma ,

8994ee6d33f19cc4ab79997cb50fa6d597801f97
usr-0123 <fixedzone365@gmail.com>
2024-01-17 08:53:37 +0300

    Operator Precedence

--- operator-precedence.js
+++ operator-precedence.js
@@ -1,17 +1,47 @@
 // OPERATOR PRECEDENCE IN JS
 //Operator precedence in JavaScript determines the order in which operators are evaluated in an expression. This means that operators with a higher precedence are evaluated first.
 //Parenthesis can be used to specify the order of evaluation, overiding the default precedence.
+
 // 1. Grouping ()
+//Parentheses () are used to achieve grouping, it explicitly specify the order of evaluation within an expression. When expressions contain parentheses, the contents within the parentheses are evaluated first,and this overides the default operator.
+
 // 2. Member Access . []
+// This is achieved by using a dot notation or a square bracket notation.
+// Dot notation (.) is used for accessing properties or methods of an Object.
+// Square brackets are used when the property or method name contains special characters, starts with a number or is determined dynamically.
+
 // 3. Function Call ()
+// Function calls are performed using parentheses ().
+
 // 4. Increment and Decrement ++ --
+// Increment and decrement are used to increase or decrease a value respectively by a value of 1. They can be used in many other ways, for example pre increment or post increment.
+
 // 5. Logical NOT !
+// It's a unary opereator that negates the truthiness of a value.
+
 // 6. Multiplication and Division * /
+// These are arithmetic operators used for basic mathematical operations.
+
 // 7. Addition and Subtraction + -
+// These are fundamental arithmetic operators used for basic mathematical operations.
+
 // 8. Relational < > <= >=
+// They are used to compare two values and return a Boolean result indicating the relationship between them.
+
 // 9. Equality == != === !==
+// Can be tested using two types of operators. The equality operator (==) and the strict equality (===).
+
 // 10. Logical AND &&
+// This is a binary operator that returns true if both of its operands are true, otherwise it returns false.
+
 // 11. Logical OR ||
+// It is a binary operator that returns true if atleast one of its operands is true, otherwise it returns false.
+
 // 12. Conditional ?:
+// This operator provides concise way to write a conditional XPathExpression. It's a shorthand of an if-else statement.
+
 // 13. Assignment = += -= *= /= %=
+// It is used to assign a value to a variable. It takes the value on it's right-hand side and assigns it to the variable on its left-hand side.
+
 // 14. Comma ,
+// This operator is used to evaluate multiple expressions in a single CSSLayerStatementRule. It evaluates each expression from left to right and returns the value of the rightmost expression.
